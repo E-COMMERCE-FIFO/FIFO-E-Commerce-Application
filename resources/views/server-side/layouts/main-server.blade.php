744e0066f9697e49da8603323484a27bf3c0906e
DANNN-GIT <e41201452@student.polije.ac.id>
2023-04-11 20:55:24 +0700

    Konfigurasi menu pada server side

--- resources/views/server-side/layouts/main-server.blade.php
+++ resources/views/server-side/layouts/main-server.blade.php
@@ -24,7 +24,7 @@
 
    <header id="header" class="header fixed-top d-flex align-items-center">
       <div class="d-flex align-items-center justify-content-between">
-         <a href="index.html" class="logo d-flex align-items-center">
+         <a href{{ url('dashboard') }}" class="logo d-flex align-items-center">
             <img src="{{ url('assets/server-side/img/logo.png') }}" alt="">
             <span class="d-none d-lg-block">NiceAdmin</span>
          </a>
@@ -97,55 +97,6 @@
                </ul>
             </li>
 
-            <li class="nav-item dropdown">
-               <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                  <i class="bi bi-chat-left-text"></i>
-                  <span class="badge bg-success badge-number">3</span>
-               </a>
-               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                  <li class="dropdown-header">
-                     You have 3 new messages <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="message-item">
-                     <a href="#">
-                        <img src="{{ url('assets/server-side/img/messages-1.jpg') }}" alt="" class="rounded-circle">
-                        <div>
-                           <h4>Maria Hudson</h4>
-                           <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                           <p>4 hrs. ago</p>
-                        </div>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="message-item">
-                     <a href="#">
-                        <img src="{{ url('assets/server-side/img/messages-2.jpg') }}" alt="" class="rounded-circle">
-                        <div>
-                           <h4>Anna Nelson</h4>
-                           <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                           <p>6 hrs. ago</p>
-                        </div>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="message-item">
-                     <a href="#">
-                        <img src="{{ url('assets/server-side/img/messages-3.jpg') }}" alt="" class="rounded-circle">
-                        <div>
-                           <h4>David Muldon</h4>
-                           <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                           <p>8 hrs. ago</p>
-                        </div>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="dropdown-footer">
-                     <a href="#">Show all messages</a>
-                  </li>
-               </ul>
-            </li>
-
             <li class="nav-item dropdown pe-3">
                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                   <img src="{{ url('assets/server-side/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
@@ -157,19 +108,19 @@
                   </li>
                   <li><hr class="dropdown-divider"></li>
                   <li>
-                     <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
                         <i class="bi bi-person"></i><span>My Profile</span>
                      </a>
                   </li>
                   <li><hr class="dropdown-divider"></li>
                   <li>
-                     <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
                         <i class="bi bi-gear"></i><span>Account Settings</span>
                      </a>
                   </li>
                   <li><hr class="dropdown-divider"></li>
                   <li>
-                     <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
                         <i class="bi bi-question-circle"></i><span>Need Help?</span>
                      </a>
                   </li>
@@ -187,217 +138,121 @@
 
    <aside id="sidebar" class="sidebar">
       <ul class="sidebar-nav" id="sidebar-nav">
+         <li class="nav-heading mb-2">Dashboards</li>
          <li class="nav-item">
-            <a class="nav-link " href="index.html">
+            <a class="nav-link" href="{{ url('dashboard') }}">
                <i class="bi bi-grid"></i><span>Dashboard</span>
             </a>
          </li>
+         <li class="nav-heading mb-2 mt-4">Data</li>
          <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-               <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#pengguna-nav" data-bs-toggle="collapse" href="">
+               <i class="bi bi-people"></i><span>Pengguna</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="pengguna-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                <li>
-                  <a href="components-alerts.html">
-                     <i class="bi bi-circle"></i><span>Alerts</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Data Pengguna</span>
                   </a>
                </li>
                <li>
-                  <a href="components-accordion.html">
-                     <i class="bi bi-circle"></i><span>Accordion</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-badges.html">
-                     <i class="bi bi-circle"></i><span>Badges</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-breadcrumbs.html">
-                     <i class="bi bi-circle"></i><span>Breadcrumbs</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-buttons.html">
-                     <i class="bi bi-circle"></i><span>Buttons</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-cards.html">
-                     <i class="bi bi-circle"></i><span>Cards</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-carousel.html">
-                     <i class="bi bi-circle"></i><span>Carousel</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-list-group.html">
-                     <i class="bi bi-circle"></i><span>List group</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-modal.html">
-                     <i class="bi bi-circle"></i><span>Modal</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-tabs.html">
-                     <i class="bi bi-circle"></i><span>Tabs</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-pagination.html">
-                     <i class="bi bi-circle"></i><span>Pagination</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-progress.html">
-                     <i class="bi bi-circle"></i><span>Progress</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-spinners.html">
-                     <i class="bi bi-circle"></i><span>Spinners</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="components-tooltips.html">
-                     <i class="bi bi-circle"></i><span>Tooltips</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Tambah Admin</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-               <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="{{ url('dashboard') }}">
+               <i class="bi bi-bus-front-fill"></i><span>Supplier</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                <li>
-                  <a href="forms-elements.html">
-                     <i class="bi bi-circle"></i><span>Form Elements</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Data Supplier</span>
                   </a>
                </li>
                <li>
-                  <a href="forms-layouts.html">
-                     <i class="bi bi-circle"></i><span>Form Layouts</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="forms-editors.html">
-                     <i class="bi bi-circle"></i><span>Form Editors</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="forms-validation.html">
-                     <i class="bi bi-circle"></i><span>Form Validation</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Tambah Supplier</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-               <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#merk-nav" data-bs-toggle="collapse" href="#">
+               <i class="bi bi-inboxes"></i><span>Merek Barang</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="merk-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                <li>
-                  <a href="tables-general.html">
-                     <i class="bi bi-circle"></i><span>General Tables</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Data Merek</span>
                   </a>
                </li>
                <li>
-                  <a href="tables-data.html">
-                     <i class="bi bi-circle"></i><span>Data Tables</span>
-                  </a>
-               </li>
-            </ul>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-               <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-               <li>
-                  <a href="charts-chartjs.html">
-                     <i class="bi bi-circle"></i><span>Chart.js</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="charts-apexcharts.html">
-                     <i class="bi bi-circle"></i><span>ApexCharts</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="charts-echarts.html">
-                     <i class="bi bi-circle"></i><span>ECharts</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Tambah Merek</span>
                   </a>
                </li>
             </ul>
          </li>
          <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-               <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#barang-nav" data-bs-toggle="collapse" href="#">
+               <i class="bi bi-boxes"></i><span>Barang</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="barang-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                <li>
-                  <a href="icons-bootstrap.html">
-                     <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Data Barang</span>
                   </a>
                </li>
                <li>
-                  <a href="icons-remix.html">
-                     <i class="bi bi-circle"></i><span>Remix Icons</span>
-                  </a>
-               </li>
-               <li>
-                  <a href="icons-boxicons.html">
-                     <i class="bi bi-circle"></i><span>Boxicons</span>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Tambah Barang</span>
                   </a>
                </li>
             </ul>
          </li>
-         <li class="nav-heading">Pages</li>
          <li class="nav-item">
-            <a class="nav-link collapsed" href="users-profile.html">
-               <i class="bi bi-person"></i>
-               <span>Profile</span>
+            <a class="nav-link collapsed" data-bs-target="#transaksi-nav" data-bs-toggle="collapse" href="#">
+               <i class="bi bi-cart-check"></i><span>Transaksi</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
+            <ul id="transaksi-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+               <li>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Pembelian</span>
+                  </a>
+               </li>
+               <li>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Penjualan</span>
+                  </a>
+               </li>
+            </ul>
          </li>
+         <li class="nav-heading mb-2 mt-4">Laporan</li>
          <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-faq.html">
-               <i class="bi bi-question-circle"></i>
-               <span>F.A.Q</span>
+            <a class="nav-link collapsed" data-bs-target="#cetak-nav" data-bs-toggle="collapse" href="#">
+               <i class="bi bi-printer"></i><span>Cetak Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
+            <ul id="cetak-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+               <li>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Barang Masuk</span>
+                  </a>
+               </li>
+               <li>
+                  <a href="{{ url('dashboard') }}">
+                     <i class="bi bi-circle"></i><span>Barang Keluar</span>
+                  </a>
+               </li>
+            </ul>
          </li>
+         <li class="nav-heading mb-2 mt-4">Lain-lain</li>
          <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-contact.html">
-               <i class="bi bi-envelope"></i>
-               <span>Contact</span>
-            </a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-register.html">
-               <i class="bi bi-card-list"></i>
-               <span>Register</span>
-            </a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-login.html">
-               <i class="bi bi-box-arrow-in-right"></i>
-               <span>Login</span>
-            </a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-error-404.html">
-               <i class="bi bi-dash-circle"></i>
-               <span>Error 404</span>
-            </a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-blank.html">
-               <i class="bi bi-file-earmark"></i>
-               <span>Blank</span>
+            <a class="nav-link collapsed" href="{{ url('dashboard') }}">
+               <i class="bi bi-sign-turn-slight-right"></i>
+               <span>Keluar</span>
             </a>
          </li>
       </ul>
