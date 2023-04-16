8d5237561899556a2994f646011e1ebe6cb9bd72
Mohammad Rizky Ramadhani <rama72720@gmail.com>
2023-04-17 01:30:31 +0700

    Add : Seeder Supplier

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+ 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use app\Models\Supplier;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,38 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+      
+        DB::table('supplier')->insert([
+            
+            [
+                'nama_supplier' => 'Ahmad Wildan',
+                'alamat' => 'Bondowoso',
+                'no_telp' => '083848139012'
+            ],
+
+            [
+                'nama_supplier' => 'Farul Ahmad Wananda',
+                'alamat' => 'Bondowoso',
+                'no_telp' => '083848139012'
+            ],
+
+            [
+                'nama_supplier' => 'M. Rizky Ramadhani',
+                'alamat' => 'Pasuruan',
+                'no_telp' => '083848139012'
+            ],
+
+            [
+                'nama_supplier' => 'Bahrul Ulum',
+                'alamat' => 'Situbondo',
+                'no_telp' => '083848139012'
+            ],
+
+            [
+                'nama_supplier' => 'Zainur Roziqin',
+                'alamat' => 'Situbondo',
+                'no_telp' => '083848139012'
+            ]
+        ]);
     }
 }
