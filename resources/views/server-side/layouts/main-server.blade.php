f145dc8640175e10bb4ad7b098ec7936e48dac21
DANNN-GIT <e41201452@student.polije.ac.id>
2023-05-29 13:42:01 +0700

    Konfigurasi tampilan dashboard admin

--- resources/views/server-side/layouts/main-server.blade.php
+++ resources/views/server-side/layouts/main-server.blade.php
@@ -30,14 +30,6 @@
          </a>
          <i class="bi bi-list toggle-sidebar-btn"></i>
       </div>
-      <div class="search-bar">
-         <form class="search-form d-flex align-items-center" method="POST" action="#">
-            <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-            <button type="submit" title="Search">
-               <i class="bi bi-search"></i>
-            </button>
-         </form>
-      </div>
       <nav class="header-nav ms-auto">
          <ul class="d-flex align-items-center">
             <li class="nav-item d-block d-lg-none">
@@ -45,89 +37,20 @@
                   <i class="bi bi-search"></i>
                </a>
             </li>
-            <li class="nav-item dropdown">
-               <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                  <i class="bi bi-bell"></i>
-                  <span class="badge bg-primary badge-number">4</span>
-               </a>
-               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                  <li class="dropdown-header">
-                     You have 4 new notifications <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="notification-item">
-                     <i class="bi bi-exclamation-circle text-warning"></i>
-                     <div>
-                        <h4>Lorem Ipsum</h4>
-                        <p>Quae dolorem earum veritatis oditseno</p>
-                        <p>30 min. ago</p>
-                     </div>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="notification-item">
-                     <i class="bi bi-x-circle text-danger"></i>
-                     <div>
-                        <h4>Atque rerum nesciunt</h4>
-                        <p>Quae dolorem earum veritatis oditseno</p>
-                        <p>1 hr. ago</p>
-                     </div>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="notification-item">
-                     <i class="bi bi-check-circle text-success"></i>
-                     <div>
-                        <h4>Sit rerum fuga</h4>
-                        <p>Quae dolorem earum veritatis oditseno</p>
-                        <p>2 hrs. ago</p>
-                     </div>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="notification-item">
-                     <i class="bi bi-info-circle text-primary"></i>
-                     <div>
-                        <h4>Dicta reprehenderit</h4>
-                        <p>Quae dolorem earum veritatis oditseno</p>
-                        <p>4 hrs. ago</p>
-                     </div>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li class="dropdown-footer">
-                     <a href="#">Show all notifications</a>
-                  </li>
-               </ul>
-            </li>
 
             <li class="nav-item dropdown pe-3">
                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                  <img src="{{ url('assets/server-side/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
+                  <img src="{{ url('assets/server-side/img/undraw_Drink_coffee_jdqb.png') }}" alt="Profile" class="rounded-circle">
                   <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->nama_lengkap }}</span>
                </a>
                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                   <li class="dropdown-header">
-                     <h6>Kevin Anderson</h6> <span>Web Designer</span>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li>
-                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
-                        <i class="bi bi-person"></i><span>My Profile</span>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li>
-                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
-                        <i class="bi bi-gear"></i><span>Account Settings</span>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li>
-                     <a class="dropdown-item d-flex align-items-center" href="{{ url('dashboard') }}">
-                        <i class="bi bi-question-circle"></i><span>Need Help?</span>
-                     </a>
+                     <h6>{{ Auth::user()->nama_lengkap }}</h6> <span>{{ Auth::user()->role }}</span>
                   </li>
                   <li><hr class="dropdown-divider"></li>
                   <li>
                      <a class="dropdown-item d-flex align-items-center" href="{{ url('logout') }}">
-                        <i class="bi bi-box-arrow-right"></i><span>Sign Out</span>
+                        <i class="bi bi-box-arrow-right"></i><span>Keluar</span>
                      </a>
                   </li>
                </ul>
