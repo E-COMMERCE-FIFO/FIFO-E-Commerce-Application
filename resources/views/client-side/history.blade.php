d77acbc61701e636fe85a8c48e1e0323b23a8142
Mohammad Rizky Ramadhani <80668526+ramadhani20@users.noreply.github.com>
2023-05-30 20:03:06 +0700

    Penambahan aksi

--- resources/views/client-side/history.blade.php
+++ resources/views/client-side/history.blade.php
@@ -13,7 +13,8 @@
         <th scope="col">Jumlah Beli</th>
         <th scope="col">Total Bayar</th>
         <th scope="col">Keterangan</th>
-        <th scope="col">Status</th>     
+        <th scope="col">Status</th>   
+        <th scope="col">Aksi</th> 
       </tr>
     </thead>
     <tbody>
@@ -23,11 +24,13 @@
         <td>{{ $item->nama_barang }}</td>
         <td>{{ $item->qty }}</td>
         <td>{{ $item->jumlah_bayar }}</td>
+
         @if ($item->status == 'Gagal' )
         <td>{{ $item->keterangan }}</td>
         @else
         <td>-</td>
         @endif
+
         @if($item->status == 'Sukses' )
         <td>
           <p class="badge bg-success">{{ $item->status }}<p>     
@@ -39,17 +42,19 @@
         @elseif($item->bukti_pembayaran == '')
         <td class="d-flex  justify-content-center align-content-center gap-2">
           <a href="{{ url('/upload/' . $item->id) }}" class="btn btn-danger ">Upload Bukti</a>
-          <form action="/delete/{{ $item->id }}"  method="post">
-            @method('delete')
-            @csrf
-          <input type="submit" value="Hapus" class="btn btn-primary">
-        </form>
         </td>
         @else
         <td>
           <p class="badge bg-primary">{{ $item->status }}<p>         
         </td>
         @endif
+        <td> 
+          <form action="/delete/{{ $item->id }}"  method="post">
+          @method('delete')
+          @csrf
+          <button type="submit" class="btn btn-danger"><i class="bi bi-trash"></i></button>
+      </form>
+      </td>
       </tr>
        @endforeach     
     </tbody>
